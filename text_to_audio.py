817151c8ce4da3864a6bf6c45b99594b73c0c9c4
KShukhrat <qayumov7298668@gmail.com>
2023-05-23 04:07:40 +0800

    instloader

---
+++ text_to_audio.py
@@ -0,0 +1,11 @@
+from gtts import gTTS
+import os
+
+text='hello uzb'
+language='en'
+
+myobj=gTTS(text=text, lang=language, slow=False)
+
+myobj.save('text.mp3')
+os.system("welcome.mp3")
+print('done')
